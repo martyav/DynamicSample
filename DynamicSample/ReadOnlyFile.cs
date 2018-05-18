2a2d4abd02ee6d53b2249812488d0ff1cf8df752
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-05-17 17:51:06 -0700

    started readonlyfile class

---
+++ DynamicSample/ReadOnlyFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Dynamic;
+
+namespace DynamicSample
+{
+    public enum StringSearchOption
+    {
+        StartsWith,
+        Contains,
+        EndsWith
+    }
+
+    class ReadOnlyFile : DynamicObject
+    {
+        private string _filePath;
+
+        public ReadOnlyFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new Exception("File path does not exist.");
+            }
+
+            _filePath = filePath;
+        }
+    }
+}
