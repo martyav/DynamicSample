e2c6c84be63eaaa86738977f6a0156668e2e182d
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-05-17 18:07:13 -0700

    filled out methods

--- DynamicSample/ReadOnlyFile.cs
+++ DynamicSample/ReadOnlyFile.cs
@@ -28,5 +28,91 @@ namespace DynamicSample
 
             _filePath = filePath;
         }
+
+        public List<string> GetPropertyValue(string propertyName,
+                                     StringSearchOption StringSearchOption = StringSearchOption.StartsWith,
+                                     bool trimSpaces = true)
+        {
+            StreamReader sr = null;
+            List<string> results = new List<string>();
+            string line = "";
+            string testLine = "";
+
+            try
+            {
+                sr = new StreamReader(_filePath);
+
+                while (!sr.EndOfStream)
+                {
+                    line = sr.ReadLine();
+
+                    // Perform a case-insensitive search by using the specified search options.
+                    testLine = line.ToUpper();
+                    if (trimSpaces) { testLine = testLine.Trim(); }
+
+                    switch (StringSearchOption)
+                    {
+                        case StringSearchOption.StartsWith:
+                            if (testLine.StartsWith(propertyName.ToUpper())) { results.Add(line); }
+                            break;
+                        case StringSearchOption.Contains:
+                            if (testLine.Contains(propertyName.ToUpper())) { results.Add(line); }
+                            break;
+                        case StringSearchOption.EndsWith:
+                            if (testLine.EndsWith(propertyName.ToUpper())) { results.Add(line); }
+                            break;
+                    }
+                }
+            }
+            catch
+            {
+                // Trap any exception that occurs in reading the file and return null.
+                results = null;
+            }
+            finally
+            {
+                if (sr != null) { sr.Close(); }
+            }
+
+            return results;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            result = GetPropertyValue(binder.Name);
+            return result == null ? false : true;
+        }
+
+        // Implement the TryInvokeMember method of the DynamicObject class for 
+        // dynamic member calls that have arguments.
+        public override bool TryInvokeMember(InvokeMemberBinder binder,
+                                     object[] args,
+                                     out object result)
+        {
+            StringSearchOption StringSearchOption = StringSearchOption.StartsWith;
+            bool trimSpaces = true;
+
+            try
+            {
+                if (args.Length > 0) { StringSearchOption = (StringSearchOption)args[0]; }
+            }
+            catch
+            {
+                throw new ArgumentException("StringSearchOption argument must be a StringSearchOption enum value.");
+            }
+
+            try
+            {
+                if (args.Length > 1) { trimSpaces = (bool)args[1]; }
+            }
+            catch
+            {
+                throw new ArgumentException("trimSpaces argument must be a Boolean value.");
+            }
+
+            result = GetPropertyValue(binder.Name, StringSearchOption, trimSpaces);
+
+            return result == null ? false : true;
+        }
     }
 }
